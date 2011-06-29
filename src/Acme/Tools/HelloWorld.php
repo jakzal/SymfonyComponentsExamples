34ff7146f713120e6f3916240c4f27757c2bfa1a
Jakub Zalas <jakub@zalas.pl>
2011-06-29 12:31:16 +0200

    Added ClassLoader example.

---
+++ src/Acme/Tools/HelloWorld.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Acme\Tools;
+
+class HelloWorld
+{
+    public function __construct()
+    {
+        echo __METHOD__."\n";
+    }
+}
---
+++ src/Legacy/Acme/Tools/HelloWorld.php
@@ -0,0 +1,9 @@
+<?php
+
+class Legacy_Acme_Tools_HelloWorld
+{
+    public function __construct()
+    {
+        echo __METHOD__."\n";
+    }
+}
