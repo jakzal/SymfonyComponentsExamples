9fe5f7558430f2206585035c2c662adfbe573969
Jakub Zalas <jakub@zalas.pl>
2011-06-30 15:08:28 +0200

    Moved Symfony components to separate vendor dir.

--- src/autoload.php
+++ src/autoload.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__.'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../vendor/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
 $loader = new Symfony\Component\ClassLoader\UniversalClassLoader();
-$loader->registerNamespaces(array('Symfony' => __DIR__));
+$loader->registerNamespaces(array('Symfony' => __DIR__.'/../vendor'));
 $loader->register();
 
