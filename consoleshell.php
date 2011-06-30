5605c980d500ee44b537b17ec8c5774d6c96c24e
Jakub Zalas <jakub@zalas.pl>
2011-06-30 20:25:00 +0200

    Added example of console shell.

---
+++ consoleshell.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/src/autoload.php';
+
+use Symfony\Component\Console as Console;
+
+$application = new Console\Application('Demo', '1.0.0');
+$application->add(new PSS\Command\HelloWorldCommand('hello-world'));
+
+$shell = new Console\Shell($application);
+$shell->run();
