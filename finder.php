2c94b4c2ed347d358449474a795756b5a45f740a
Jakub Zalas <jakub@zalas.pl>
2011-07-08 10:43:16 +0200

    Fixed letter bug (untill -> until).

--- finder.php
+++ finder.php
@@ -8,11 +8,11 @@ $files = Finder\Finder::create()
     ->files()
     ->name('/^He.+Command.php$/')
     ->size('<4k')
-    ->date('untill yesterday')
+    ->date('until yesterday')
     ->sortByName()
     ->in('.');
 
-echo "Command files starting with 'He' below 4k modified untill yesterday:\n";
+echo "Command files starting with 'He' below 4k modified until yesterday:\n";
 foreach ($files as $file) {
     printf("* %s %s\n", $file->getFilename(), date('Y-m-d H:i', $file->getMTime()));
 }
